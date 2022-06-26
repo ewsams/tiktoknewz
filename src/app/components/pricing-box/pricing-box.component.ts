892b316b591c9395fac24cda20476809589ce677
edward sams <40345707+ewsams@users.noreply.github.com>
2022-06-26 15:51:32 -0400

    Add files via upload

---
+++ src/app/components/pricing-box/pricing-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pricing-box',
+  templateUrl: './pricing-box.component.html',
+  styleUrls: ['./pricing-box.component.scss']
+})
+export class PricingBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
